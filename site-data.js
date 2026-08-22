becbe1ac44ffa443bd1273982412dc97f6f2bfef
Cezara Arcalean <103682119+CezaraArcalean@users.noreply.github.com>
2026-08-22 21:33:46 +0300

    Add maintainable public site data config

---
+++ site-data.js
@@ -0,0 +1,38 @@
+window.HeartifactsSiteData = {
+  social: {
+    instagram: "https://www.instagram.com/heartifacts_band/",
+    spotify: "",
+    youtube: "https://www.youtube.com/@Heartifacts-band"
+  },
+
+  contact: {
+    email: ""
+  },
+
+  hero: {
+    image: "",
+    alt: {
+      en: "Heartifacts band photo",
+      ro: "Fotografie cu trupa Heartifacts"
+    }
+  },
+
+  featuredRelease: {
+    title: "Red Flag",
+    artwork: "",
+    spotify: "",
+    youtube: "https://www.youtube.com/watch?v=FbAdPtLhO2g"
+  },
+
+  shows: [
+    {
+      date: "",
+      venue: "The Pub",
+      city: "Bucharest",
+      eventUrl: "",
+      ticketUrl: ""
+    }
+  ],
+
+  gallery: []
+};
